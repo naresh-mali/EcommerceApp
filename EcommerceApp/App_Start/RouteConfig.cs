db4f04c34d24093e2561737a977c0d854759a032
Naresh Mali <nareshm.techno@gmail.com>
2023-01-20 00:02:34 +0530

    20 Jan 2023 Changes

--- EcommerceApp/App_Start/RouteConfig.cs
+++ EcommerceApp/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace EcommerceApp
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            routes.MapRoute(
+            name: "Login",
+            url: "Login/{action}/{id}",
+            defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
+        );
+
         }
     }
 }
